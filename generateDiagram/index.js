553ef9f1f09f626bfccdb48489762224a8a6ea20
Tim Garvin <tcgarvin@gmail.com>
2023-11-11 01:19:21 -0500

    Introduce mermaid-cli import and slowly move function

--- generateDiagram/index.js
+++ generateDiagram/index.js
@@ -1,13 +1,16 @@
+import { run } from "@mermaid-js/mermaid-cli"
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const mermaidCode = `
+sequenceDiagram
+    ${name} ->> Other: Hey
+    Other ->> ${name}: Yo
+`
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: mermaidCode
     };
 }
